01004636e54c21d210587931462a64a96e055804
ArkhamCookie <git@mail.theuntitledproject.dev>
2024-09-05 00:14:24 -0500

    Feat: Complete chapter 25

---
+++ src/control-flow/switch/Main.java
@@ -0,0 +1,84 @@
+public class Main {
+	static void sayColor(String fruit) {
+		switch (fruit) {
+			case "apple" -> {
+				System.out.println("Red");
+			}
+			case "grape" -> {
+				System.out.println("Purple");
+			}
+			case "orange" -> {
+				System.out.println("Orange");
+			}
+			default -> {
+				System.out.println("Other");
+			}
+		}
+	}
+
+	static void checkCorrectYear(int year) {
+		switch (year) {
+			case 2023 -> {
+				System.out.println("One year too early");
+			}
+			case 2024 -> {
+				System.out.println("Correct year!");
+			}
+			case 2025 -> {
+				System.out.println("One year too late.");
+			}
+			default -> {
+				System.out.println("Not the correct year.");
+			}
+		}
+	}
+
+	enum StopLight {
+		RED,
+		YELLOW,
+		GREEN
+	}
+
+	static void checkIfGo(StopLight light) {
+		switch (light) {
+			case GREEN -> {
+				System.out.println("Go!");
+			}
+			case YELLOW -> {
+				System.out.println("Speed up, coward!");
+			}
+			case RED -> {
+				System.out.println("Stop!");
+			}
+			default -> {
+				System.out.println("Error!");
+			}
+		}
+	}
+
+	static String combingCases(String vegetable) {
+		switch (vegetable) {
+			case "apple" -> {
+				return "Malus pumila";
+			}
+			case "cabbage", "brussel sprouts", "kale", "cauliflower" -> {
+				return "Brassica oleracea";
+			}
+			case null, default -> {
+				return "unknown";
+			}
+		}
+	}
+
+	public static void main(String[] args) {
+		sayColor("apple");
+
+		checkCorrectYear(2023);
+
+		var light = StopLight.YELLOW;
+
+		checkIfGo(light);
+
+		System.out.println(combingCases("kale"));
+	}
+}
